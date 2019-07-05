da78d2cdbe69413b4db38b4d699d4f64998bf9f8
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-07-05 17:51:36 +0200

    Skip the stars resort task on the initialisation step.

--- src/engine.c
+++ src/engine.c
@@ -3327,6 +3327,7 @@ void engine_skip_force_and_kick(struct engine *e) {
         t->type == task_type_extra_ghost ||
         t->type == task_type_bh_swallow_ghost1 ||
         t->type == task_type_bh_swallow_ghost2 ||
+        t->type == task_type_stars_resort ||
         t->subtype == task_subtype_gradient ||
         t->subtype == task_subtype_stars_feedback ||
         t->subtype == task_subtype_bh_feedback ||
